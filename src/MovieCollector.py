6c4dade2e5dfbb16d323551870fa788b23eb6dad
jinouwuque <ee2win@gmail.com>
2017-02-05 16:51:48 -0500

    better recursive logic

--- src/MovieCollector.py
+++ src/MovieCollector.py
@@ -2,7 +2,9 @@ import os
 import re
 import csv
 
-defaultReg = "^[a-zA-Z]{3,7}(|-)[0-9]{3,5}"
+default_movie_ext = ['.mp4', '.avi', '.mkv']
+defaultReg = "^[a-zA-Z]{3,8}(|-|_)[0-9]{2,6}"
+defaultReg2 = "^(|1|10)[a-zA-Z]{3,8}(|-|_)[0-9]{2,6}"
 
 class MovieCollector:
     def __init__(self, pathList, showDuplicate, regEx = defaultReg):
@@ -28,20 +30,25 @@ class MovieCollector:
                 })
 
     def addToList(self, list, name, dir):
+        shortName = re.search(self.regStr, name).group(0)
+        name_grp = re.split('(\d.*)', shortName)
+        list.append({
+            "c": name_grp[0],
+            "i": name_grp[1],
+            "d": dir
+        })
+
+    def check_match(self, str):
         p = re.compile(self.regStr)
-        if p.match(name):
-            shortName = re.search(self.regStr, name).group(0)
-            name_grp = re.split('(\d.*)', shortName)
-            list.append({
-                "c": name_grp[0],
-                "i": name_grp[1],
-                "d": dir
-            })
+        return p.match(str)
 
     def change_format(self, str):
         str1 = str.replace("-", "")
         str2 = str1.lower()
         return str2
+    def is_movie(self, name):
+        ext = os.path.splitext(name)[1]
+        return ext in default_movie_ext
 
     def start(self):
         list = []
@@ -49,19 +56,22 @@ class MovieCollector:
             print('analysizing directory(%s)...' % (cwd))
 
             for root, dirs, files in os.walk(cwd):
-                if len(dirs) < 1:
-                    # normal folders
-                    fanName0 = os.path.basename(root)
-                    fanName = self.change_format(fanName0)
-                    self.addToList(list, fanName, root)
-                elif len(files) > 1 and len(dirs) > 0:
-                    # files without parent folder
-                    fanName = os.path.basename(root)
+
+                folder_name = os.path.basename(root)
+                folder_name1 = self.change_format(folder_name)
+                if self.check_match(folder_name1):
+                    self.addToList(list, folder_name1, root)
+                else:
                     for oneFile in files:
-                        fileName = os.path.splitext(oneFile)[0]
-                        if fileName != '.DS_Store':
-                            name = self.change_format(os.path.splitext(oneFile)[0])
+                        if not self.is_movie(oneFile):
+                            continue
+
+                        name = self.change_format(os.path.splitext(oneFile)[0])
+                        if self.check_match(name):
                             self.addToList(list, name, root)
+
+
+
 
 
         list.sort(key=lambda x: x['c'] + x['i'])
